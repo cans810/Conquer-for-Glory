8836f3072dd762ee8f591915efea221b07602d2e
Can Selçuk <canselcuk810@gmail.com>
2024-02-17 20:19:19 +0300

    en son dokunuşlar

---
+++ .history/Assets/Scripts/MainMenuScripts/MainMenuManager_20240217010459.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject creditsGameObject;
+    public GameObject mainMenuUI;
+    public GameObject loadGameUI;
+    public GameObject howToUI;
+
+    public void Awake(){
+        creditsGameObject.SetActive(false);
+        loadGameUI.SetActive(false);
+        howToUI.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGameButton(){
+        mainMenuUI.SetActive(false);
+        creditsGameObject.SetActive(false);
+        howToUI.SetActive(false);
+        loadGameUI.SetActive(true);
+        loadGameUI.GetComponent<LoadSoldiers>().LoadGame();
+    }
+
+    public void showCreditsScreen(){
+        creditsGameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void showHowToScreen(){
+        howToUI.SetActive(true);
+        creditsGameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    public void goBack(){
+        creditsGameObject.SetActive(false);
+        gameObject.SetActive(true);
+    }
+
+    public void advanceToRaceSelection(){
+        DataPersistanceManager.instance.NewGame();
+        SceneManager.LoadScene("RaceSelectionScene");
+    }
+}
